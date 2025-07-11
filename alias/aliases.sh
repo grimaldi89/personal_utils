cb977108a538eb0d8e9f9d96f70daba0d634d62d
Rodolfo Grimaldi <33096202+grimaldi89@users.noreply.github.com>
2025-07-11 08:14:12 -0300

    Add utils alias and docs

--- alias/aliases.sh
+++ alias/aliases.sh
@@ -4,5 +4,6 @@ alias python='python3'
 alias bashrc='gedit ~/.bashrc && source ~/.bashrc'
 alias new_venv='~/personal_utils/alias/code/python_venv.sh'
 alias persist_env='bash ~/personal_utils/alias/code/persist_env.sh'
+alias utils='bash ~/personal_utils/install.sh'
 
 
